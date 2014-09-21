6d2c2944a1b25a98ba9fea24a7155ee221c3b017
Bardo91 <19Bardo91@gmail.com>
2014-09-22 01:11:57 +0200

    Implemented cstdint

---
+++ src/stl/cstdint.h
@@ -0,0 +1,26 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//		avr_base library
+//			Author: Pablo Ramon Soria
+//			Date:	2014/09/22
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+#ifndef _AVR_BASE_STL_CSTDINT_H_
+#define _AVR_BASE_STL_CSTDINT_H_
+
+namespace std{
+	typedef unsigned char	uint8_t;
+	typedef signed char		int8_t;
+	typedef unsigned int	uint16_t;
+	typedef signed int		int16_t;
+	typedef unsigned long	uint32_t;
+	typedef signed long		int32_t;
+
+
+}	//	namespace std
+
+
+#endif	//	_AVR_BASE_STL_CSTDINT_H_
