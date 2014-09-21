d0318d9f58e2e0011590a52a93fa119607910068
Bardo91 <19Bardo91@gmail.com>
2014-09-22 01:40:55 +0200

    Defined basic interface for Time class

--- src/core/time/Time.h
+++ src/core/time/Time.h
@@ -11,16 +11,26 @@
 #ifndef _AVR_BASE_CORE_TIME_H_
 #define _AVR_BASE_CORE_TIME_H_
 
-#include "../hal/timer/Timer.h"
+#include "../../hal/timer/Timer.h"
+#include <cstdint>
 
 namespace avr_base{
 	class Time{		// Singleton class with utils about time. Use Timer 1
 	public:
+		static void init();
+
 		static void		delayUs(unsigned _micros);
 		static void		delayMs(unsigned _millis);
+				
+		static unsigned seconds();
+		static unsigned millis();
+		static unsigned micros();
 
-		static void init();
+		static void tickTimer();
 	private:
+		static volatile std::uint32_t sMillis;
+		static volatile std::uint32_t sMicros;
+
 		static hal::Timer1 sTimer;
 
 	};
